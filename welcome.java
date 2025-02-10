fe27602a6c8ce6137bf16d60718bb5f0f7156dff
Dyooooou <juliandiokhusussekolah@gmail.com>
2025-02-10 13:10:18 +0700

    versi 1

---
+++ welcome.java
@@ -0,0 +1,9 @@
+public class welcome{
+    public static void main(String[] args){
+        String greeting = "Welcome to my Core Java";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++)
+            System.out.print("=");
+        System.out.println();
+    }
+}
