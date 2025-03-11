297e45316cb8b50503b78849fdadf2aa3105b242
Dyooooou <juliandiokhusussekolah@gmail.com>
2025-03-11 14:42:52 +0700

    Update surat.java

--- Per_5/surat.java
+++ Per_5/surat.java
@@ -1,1 +1,21 @@
 
+import java.util.List;
+import java.util.ArrayList;
+
+public class surat{
+    private String surat;
+    public void suratPendek(){
+            List<String> surat = new ArrayList<>();
+        
+            surat.add("Al-Ikhlas");
+            surat.add("AL-Falaq");
+            surat.add("An-nas");
+            surat.add("Al-Kafirun");
+            surat.add("Al-Kautsar");
+            surat.add("Al-Mau'un");
+
+            System.out.println("Surat pendek yang tersedia untuk dibaca:");
+            System.out.println(surat);
+        }
+
+}
